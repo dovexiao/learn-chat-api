98f2b56907292170e9812a198a9a77f635d157ec
xjf24 <2867179160@qq.com>
2025-04-12 04:34:23 +0800

    修改部分auth参数校验体, 补充刷新token的参数校验

--- src/utils/validators/auth.validator.ts
+++ src/utils/validators/auth.validator.ts
@@ -3,11 +3,17 @@ import Joi from 'joi';
 export const registerSchema = Joi.object({
     username: Joi.string().min(3).required(),
     password: Joi.string().min(6).required(),
-    captcha: Joi.string().required()
+    captchaKey: Joi.string().required(),
+    captchaText: Joi.string().required()
 });
 
 export const loginSchema = Joi.object({
     username: Joi.string().required(),
     password: Joi.string().required(),
-    captcha: Joi.string().required()
+    captchaKey: Joi.string().required(),
+    captchaText: Joi.string().required()
+});
+
+export const refreshSchema = Joi.object({
+    refreshToken: Joi.string().required()
 });
